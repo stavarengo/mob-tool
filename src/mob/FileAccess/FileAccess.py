728f8416cf2fc9c0a8841132b3a778d6d5eabf90
Rafael Stavarengo <rafael.stavarengo@mollie.com>
2023-02-04 17:43:37 +0100

    Create FileAccess.py

---
+++ src/mob/FileAccess/FileAccess.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from injector import inject
+
+
+@inject
+@dataclass
+class FileAccess:
+
+    def read(self, file_path: str, fail_if_not_found: bool = False) -> str | None:
+        try:
+            with open(file_path, 'r') as f:
+                return f.read()
+        except FileNotFoundError as e:
+            if fail_if_not_found:
+                raise e
+            return None
+
+    def save(self, content: str, file_path: str) -> None:
+        with open(file_path, 'w') as f:
+            f.write(content)
