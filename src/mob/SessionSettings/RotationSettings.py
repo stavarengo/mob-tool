2c2e9542f8fb7238d616ee71a6ad8de93e9edeb7
Rafael Stavarengo <rafael.stavarengo@mollie.com>
2023-02-15 21:38:59 +0100

    setup.py

--- src/mob/SessionSettings/RotationSettings.py
+++ src/mob/SessionSettings/RotationSettings.py
@@ -6,6 +6,6 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass(frozen=True)
 class RotationSettings:
-    driverInMinutes: float = 10
-    breakInMinutes: float = 15
+    driverInMinutes: int = 10
+    breakInMinutes: int = 15
     howManyRotationsBeforeBreak: int = 6
