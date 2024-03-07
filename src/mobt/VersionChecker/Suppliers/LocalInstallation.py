b65280217324e7b179abbfbe8e0fa5b2ecdde429
stavarengo <rafael.stavarengo@mollie.com>
2024-03-07 16:39:45 +0100

    Using importlib

--- src/mobt/VersionChecker/Suppliers/LocalInstallation.py
+++ src/mobt/VersionChecker/Suppliers/LocalInstallation.py
@@ -1,20 +1,26 @@
 from dataclasses import dataclass
 from typing import Optional
 
-import pkg_resources
+# Updated imports
+from importlib.metadata import version, PackageNotFoundError
 from injector import inject
 from packaging.version import Version
 
 from mobt.VersionChecker import version_checker_logger
 from mobt.VersionChecker.Suppliers.SupplierInterface import SupplierInterface
 
-
 @inject
 @dataclass(frozen=True)
 class LocalInstallation(SupplierInterface):
 
     def get_version(self) -> Optional[Version]:
-        version = pkg_resources.get_distribution('mob-tool').version
-        version_checker_logger().debug(f'Version installed locally: {version}')
+        try:
+            # Using importlib.metadata.version to get the package version
+            version_str = version('mob-tool')
+            version_checker_logger().debug(f'Version installed locally: {version_str}')
 
-        return Version(version)
+            return Version(version_str)
+        except PackageNotFoundError:
+            # Handle the case where the package is not found
+            version_checker_logger().debug('mob-tool package not found locally.')
+            return None
